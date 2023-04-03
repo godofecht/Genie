7f4e1043ccee2bb665888050203690b1360e6042
godofecht <abhishek.shivakumar@email.com>
2023-04-03 22:29:30 +0100

    u

--- config/database.php
+++ config/database.php
@@ -33,7 +33,7 @@ return [
     |
     */
 
-    'DB_CONNECTION' => [
+    'connections' => [
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -42,7 +42,7 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-/*
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -62,15 +62,14 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-*/
 
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '8000'),
-            'database' => env('DB_DATABASE', 'genie'),
-            'username' => env('DB_USERNAME', 'postgrese'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
@@ -147,3 +146,4 @@ return [
 
 ];
 
+DB_CONNECTION=pgsql
