bf2c68047cbf1b5f7614201653b097bf30b38a2d
godofecht <abhishek.shivakumar@email.com>
2023-04-03 22:18:10 +0100

    u

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'genie'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
@@ -145,6 +145,4 @@ return [
     ],
 
 ];
-
-DB_DATABASE=genie
 
